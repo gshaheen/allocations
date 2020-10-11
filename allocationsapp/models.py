4c50c3a2b20689e718fabfd3ab98f3089df0c4a0
George Shaheen <gshaheen@gmail.com>
2020-10-11 11:56:15 -0600

    added portfolio and allocation models

--- allocationsapp/models.py
+++ allocationsapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Portfolio(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Allocation(models.Model):
+    asset = models.CharField(max_length=100)
+    percentAllocation = models.PositiveIntegerField()
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.asset
